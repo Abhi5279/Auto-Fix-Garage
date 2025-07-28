9f836d285b867aa9f88ed7cd07d78285b8a12706
Pachigolla Abhishek <168338524+Abhi5279@users.noreply.github.com>
2025-07-28 16:22:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,8 @@ const app = express();
 const port = 3000;
 
 // CORS config
-const allowedOrigins = ['http://localhost:5173', 'https://garage-management-site-frontend.onrender.com'];
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: 'https://garage-management-site-frontend.onrender.com',
     credentials: true
 }));
 
